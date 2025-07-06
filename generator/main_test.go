aaa96165f08bcbbcba1cf834b0b6fe1987c8ac88
Sels@qvest <sels@tarent.qvest>
2025-07-06 07:30:31 +0200

    update

---
+++ generator/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGenerateTerraformFromJSON(t *testing.T) {
+	input := `{"properties": {"name": "test-monitor"}}`
+	inputFile := "test_monitor.json"
+	outputFile := "test_monitor.tf"
+	os.WriteFile(inputFile, []byte(input), 0644)
+	defer os.Remove(inputFile)
+	defer os.Remove(outputFile)
+	err := GenerateTerraformFromJSON(inputFile, outputFile)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	content, err := os.ReadFile(outputFile)
+	if err != nil {
+		t.Fatalf("Failed to read output: %v", err)
+	}
+	if len(content) == 0 {
+		t.Error("Output Terraform file is empty")
+	}
+}
