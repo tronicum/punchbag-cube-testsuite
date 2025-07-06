28c71bf3100aec97a2919a0c745061076f048fb5
Sels@qvest <sels@tarent.qvest>
2025-07-06 07:45:18 +0200

    files

---
+++ generator/test_aks_expanded.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "test-aks"
+  location            = "eastus"
+  resource_group_name = "test-rg"
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+  }
+  // ...map more fields from JSON as needed
+}
